317254b6d4421106efb1243bac9218d5d9d975a9
vijay singh dobal <vijaydobal23@gmail.com>
2026-07-03 11:49:37 +0530

    Update auth cookie to include secure: true

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,13 +37,14 @@ export const registerUser = async (req, res) => {
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000, 
         sameSite: "none",
+        secure:true
       });
 
       res.status(201).json({
         _id: user._id,
         fullName: user.fullName,
         email: user.email,
-        token: token // Returning token in body as well for fallback
+        token: token 
       });
     } else {
       res.status(400).json({ message: 'Invalid user data' });
@@ -72,6 +73,7 @@ export const loginUser = async (req, res) => {
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         sameSite:"none",
+        secure:true
       });
 
       res.json({
@@ -90,9 +92,11 @@ export const loginUser = async (req, res) => {
 
 
 export const logoutUser = (req, res) => {
+  
   res.cookie('token', '', {
     httpOnly: true,
     expires: new Date(0),
+   
   });
   res.status(200).json({ message: 'Logged out successfully' });
 };
